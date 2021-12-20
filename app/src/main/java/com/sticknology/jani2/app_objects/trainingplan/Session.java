1436772396787c5f6d2de707508f23d9ac71cf67
phansen262 <peterhansen1198@gmail.com>
2021-12-20 14:30:11 -0600

    Update Session.java

--- app/src/main/java/com/sticknology/jani2/app_objects/trainingplan/Session.java
+++ app/src/main/java/com/sticknology/jani2/app_objects/trainingplan/Session.java
@@ -1,4 +1,25 @@
 package com.sticknology.jani2.app_objects.trainingplan;
 
 public class Session {
+  
+  private String mName;
+  private String mDescription;
+  private String mType;
+  
+  private List<Exercise> mExerciseList = new ArrayList();
+  private List<Container> mAttributes = new ArrayList();
+  
+  //Base Constructor
+  public Session(String name, String description, String type, List<Exercise> exerciseList, List<Container> attributes){
+    
+    mName = name;
+    mDescription = description;
+    mType = type;
+    
+    mExerciseList = exerciseList;
+    mAttributes = attributes;
+  }
+  
+  //Basic Getters and Setters
+  
 }
