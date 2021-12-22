684684dc549d68b156a19df556e1911d61971a52
phansen262 <peterhansen1198@gmail.com>
2021-12-22 14:36:55 -0600

    Update TrainingPlan.java

--- app/src/main/java/com/sticknology/jani2/app_objects/trainingplan/TrainingPlan.java
+++ app/src/main/java/com/sticknology/jani2/app_objects/trainingplan/TrainingPlan.java
@@ -1,4 +1,23 @@
 package com.sticknology.jani2.app_objects.trainingplan;
 
 public class TrainingPlan {
+  
+  private String mName;
+  private String mDescription;
+  
+  private MDay mStartDay;
+  private MDay mEndDay;
+  
+  private List<Tuple> mDayTypes = new ArrayList<Tuple>();
+  private List<Carrier> mAttributes = new ArrayList<Carrier>();
+  
+  public TrainingPlan(name, description, startDay, endDay, dTypes, attributes){
+    
+    mName = name;
+    mDescription = description;
+    mStartDay = startDay;
+    mEndDay = endDay;
+    mDayTypes = dayTypes;
+    mAttributes = attributes
+  }
 }
