46ef69bedf7112ddaad5e09dfe7bfb4b20bf6735
Peter Hansen <peterhansen1198@gmail.com>
2022-01-15 12:37:17 -0600

    Before introducing exercise id's

--- app/src/main/java/com/sticknology/jani2/app_objects/trainingplan/exercises/Exercise.java
+++ app/src/main/java/com/sticknology/jani2/app_objects/trainingplan/exercises/Exercise.java
@@ -9,6 +9,7 @@ public class Exercise implements Serializable {
     private String mName;
     private String mDescription;
     private String mType;
+    private int mID;
 
     private HashMap<String, List<String>> mAttributes;
 
@@ -26,19 +27,28 @@ public class Exercise implements Serializable {
         mAttributes = attributes;
     }
 
+    public Exercise(String name, String description, String type, HashMap<String, List<String>> attributes, int id){
+
+        mName = name;
+        mDescription = description;
+        mType = type;
+        mAttributes = attributes;
+        mID = id;
+    }
+
     //Baseline get methods
     public String getName(){
         return mName;
     }
-
     public String getDescription(){return mDescription;}
-
     public String getType(){return mType;}
+    public int getID(){return mID;}
 
     //Baseline set methods
     public void setName(String name){mName = name;}
     public void setDescription(String description){mDescription = description;}
     public void setType(String type){mType = type;}
+    public void setID(int id){mID = id;}
 
     //Interact with Attributes
     public void addAttribute(String key, List<String> payload){
